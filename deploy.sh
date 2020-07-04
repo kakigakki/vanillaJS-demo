2d2fc9ea4419da88ff6e0104214611542337a961
kakigakki <kakigakki@yahoo.co.jp>
2020-07-04 18:46:25 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+git push -f git@github.com:kakigakki/vanillaJS-demo.git master:gh-pages
+
+cd -
